ac2ede9634d4a824c4f0c825f65351d03c0d22da
Jovane Castro <jovanecastro12@gmail.com>
2021-04-22 01:36:46 -0300

    Finished crono

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(() => {
   const modalWindow = $(".modal");
 
   $("button").click(() => {
+    //Time input and validation
     let minutes = parseInt($("#min").val());
     let seconds = parseInt($("#sec").val());
     if (!minutes && seconds) minutes = 0;
@@ -11,28 +12,45 @@ $(() => {
     if (!minutes && !seconds) {
       alert("You're supposed to input some time.");
     } else {
+      //Fade-in of modal window
       mainWindow.fadeOut();
       modalWindow.fadeIn();
 
+      //60-seconds-to-1-minute conversion
       if (seconds > 60) {
         minutes += parseInt(seconds / 60);
         seconds = seconds % 60;
       }
 
-      function stringTime(x, y) {
-        x = String(x);
-        y = String(y);
-        if (x.length === 1) x = ("0" + x).slice(-2);
-        if (y.length === 1) y = ("0" + y).slice(-2);
+      //Stringify variable
+      function stringTime(x) {
+        let a = String(x);
+        if (a.length < 2) a = ("0" + a).slice(-2);
+        return a;
       }
-      stringTime(minutes, seconds);
-      console.log(`Minutes: ${minutes}\nSeconds: ${seconds}`);
-      $(".black p").text(`${minutes}:${seconds}`);
-    }
-  });
 
-  $(".x").click(() => {
-    modalWindow.fadeOut();
-    mainWindow.fadeIn();
+      $(".black p").text(`${stringTime(minutes)}:${stringTime(seconds)}`);
+
+      //Timer function
+      function crono() {
+        if (minutes > 0 && seconds === 0) {
+          minutes -= 1;
+          seconds += 60;
+        }
+        seconds -= 1;
+        console.log(minutes + ":" + seconds);
+        if (minutes === 0 && seconds === 0) clearInterval(timer);
+        $(".black p").text(`${stringTime(minutes)}:${stringTime(seconds)}`);
+      }
+      //Starting timer
+      let timer = setInterval(crono, 100);
+
+      //Fade-out modal window
+      $(".x").click(() => {
+        modalWindow.fadeOut();
+        mainWindow.fadeIn();
+        clearInterval(timer);
+      });
+    }
   });
 });
