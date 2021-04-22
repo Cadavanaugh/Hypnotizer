b1411671abb1af6bc95dde4680c3edde07049cc0
Jovane Castro <jovanecastro12@gmail.com>
2021-04-22 01:06:37 -0300

    Start of crono-logic

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 $(() => {
   const mainWindow = $(".main");
   const modalWindow = $(".modal");
+
   $("button").click(() => {
-    mainWindow.css("display", "none");
-    modalWindow.css("display", "flex");
+    let minutes = parseInt($("#min").val());
+    let seconds = parseInt($("#sec").val());
+    if (!minutes && seconds) minutes = 0;
+    if (minutes && !seconds) seconds = 0;
+
+    if (!minutes && !seconds) {
+      alert("You're supposed to input some time.");
+    } else {
+      mainWindow.fadeOut();
+      modalWindow.fadeIn();
+
+      if (seconds > 60) {
+        minutes += parseInt(seconds / 60);
+        seconds = seconds % 60;
+      }
+
+      function stringTime(x, y) {
+        x = String(x);
+        y = String(y);
+        if (x.length === 1) x = ("0" + x).slice(-2);
+        if (y.length === 1) y = ("0" + y).slice(-2);
+      }
+      stringTime(minutes, seconds);
+      console.log(`Minutes: ${minutes}\nSeconds: ${seconds}`);
+      $(".black p").text(`${minutes}:${seconds}`);
+    }
   });
+
   $(".x").click(() => {
-      mainWindow.css("display",'block')
-      modalWindow.css("display",'none')
+    modalWindow.fadeOut();
+    mainWindow.fadeIn();
   });
 });
